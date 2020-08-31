46c3c0b881d470f9773d4f77b39e12527c8d2014
Per Kristian Svanberg <kristiansvanberg@gmail.com>
2020-08-31 09:28:53 +0200

    more cleanup and WIP

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
-import './App.css';
+import {MenuItem, FormControl, Select} from '@material-ui/core';
+import './styles/App.css';
 
 function App() {
   return (
     <div className="app">
-      <h1>building a covid 19 tracker</h1>
+      <div className="app__header">
+        <h1>COVID-19 TRACKER</h1>
+        <FormControl className="app__dropdown">
+          <Select
+            variant="outlined"
+            value="abc"
+          >
+            <MenuItem value="worldwide">Worldwide</MenuItem>
+            <MenuItem value="worldwide">Option two</MenuItem>
+            <MenuItem value="worldwide">Option 3</MenuItem>
+            <MenuItem value="worldwide">YOOOOOO</MenuItem>
+          </Select>
+        </FormControl>
+      </div>
     </div>
   );
 }
