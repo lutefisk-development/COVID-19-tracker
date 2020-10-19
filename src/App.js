14a27f9fde1bba044db0b0a22a954c8da04acf6c
lutefisk-development <kristiansvanberg@gmail.com>
2020-10-19 14:01:27 +0200

    Added some styling to the header. Also made API call to fetch countries from "diseases.sh" and rendered them out.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {MenuItem, FormControl, Select} from '@material-ui/core';
 import './styles/App.css';
 
 function App() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch('https://disease.sh/v3/covid-19/countries')
+      .then(response => response.json())
+      .then(data => {
+        const countries = data.map(country => (
+          {
+            name: country.country,
+            value: country.countryInfo.iso2,
+          }
+        ));
+
+        setCountries(countries);
+      });
+    };
+
+    getCountriesData();
+  }, []);
+
   return (
     <div className="app">
       <div className="app__header">
         <h1>COVID-19 TRACKER</h1>
         <FormControl className="app__dropdown">
-          <Select
-            variant="outlined"
-            value="abc"
-          >
-            <MenuItem value="worldwide">Worldwide</MenuItem>
+          <Select variant="outlined" value="abc">
+
+            {
+              countries.map(country => (
+              <MenuItem value={country.value}>{country.name}</MenuItem>
+              ))
+            }
+
+            {/* <MenuItem value="worldwide">Worldwide</MenuItem>
             <MenuItem value="worldwide">Option two</MenuItem>
             <MenuItem value="worldwide">Option 3</MenuItem>
-            <MenuItem value="worldwide">YOOOOOO</MenuItem>
+            <MenuItem value="worldwide">YOOOOOO</MenuItem> */}
           </Select>
         </FormControl>
       </div>
